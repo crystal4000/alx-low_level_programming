4f4342e70991d4706f7afdfef85f1b5f0ee4f7d9
Nefertiti <fredrickamanda3@gmail.com>
2023-04-21 06:46:02 -0700

    print Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return 0;	
+}
