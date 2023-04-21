eb17e6662a838776ccc026f428ce5bec2948cdc4
Nefertiti <fredrickamanda3@gmail.com>
2023-04-21 08:14:24 -0700

    Print_Alphabt.c git commit -m Print_Alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
