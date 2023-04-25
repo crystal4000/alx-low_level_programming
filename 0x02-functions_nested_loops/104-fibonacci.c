c00eec86835d797189a2b9468ff2c1814254c9dc
Nefertiti <fredrickamanda3@gmail.com>
2023-04-25 12:27:35 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,10 +14,11 @@ int main(void)
 	printf("%.0f, %.0f, ", a, b);
 	while (count < 98)
 	{
+		count++;
 		printf("%.0f, ", c);
 		a = b;
 		b = c;
-		count++;
+		c = a + b;
 	}
 	printf("%.0f\n", a + b);
 	return (0);
