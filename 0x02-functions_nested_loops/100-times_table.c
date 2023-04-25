eeac1f14e780a6a2ec06afed8ec33d0909aa1f4e
Nefertiti <fredrickamanda3@gmail.com>
2023-04-25 09:10:23 -0700

    Task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - times table function
+ * @n: integer to return
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n < 0 || n >15)
+	{
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int product = i * j;
+
+			if (product < 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			} else if (product < 100)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			} else
+			{
+				_putchar(' ');
+				_putchar((product % 100) / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}	
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "6-abs.c"
 #include <stdio.h>
 
 /**
