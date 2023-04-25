a2e1bf83303b2c6b12707c8edc1fe1d02cd69c3c
Nefertiti <fredrickamanda3@gmail.com>
2023-04-25 09:45:44 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,41 +13,39 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 	{
-		return;
-	}
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			int product = i * j;
-                        
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
+				int product = i * j;
+                        
+				if (j == 0)
+				{
+					_putchar('0');
+			} else if (product <= 9)
+			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(product / 10 + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			} else if (product > 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 100 + '0');
+				_putchar((product / 10) % 10 + '0');
 				_putchar(product % 10 + '0');
 			} else
 			{
 				_putchar(',');
 				_putchar(' ');
-			if (product < 9)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product + '0');
-			} else if (product < 99)
-			{
-				_putchar(' ');
+				_putchat(' ')'
 				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-			} else
-			{
-				_putchar(product / 10 + '0');
-				_putchar((product / 10) % 10 + '0');
 				_putchar(product % 10 + '0');
 			}
 		}
-	}
 		_putchar('\n');
+		}
 	}
-}	
+}
