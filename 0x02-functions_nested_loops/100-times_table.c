7579d1f6665f9d3605c95f3e3ddf7757094e68fb
Nefertiti <fredrickamanda3@gmail.com>
2023-04-25 09:23:25 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,26 +19,33 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			int product = i * j;
-
+                        
+			if (j == 0)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			} else
+			{
+				_putchar(',');
+				_putchar(' ');
 			if (product < 9)
 			{
-				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
 			} else if (product < 100)
 			{
 				_putchar(' ');
-				_putchar(' ');
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
 			} else
 			{
-				_putchar(' ');
-				_putchar((product % 100) / 10 + '0');
+				_putchar(product / 10 + '0');
+				_putchar((product / 100) % 10 + '0');
 				_putchar(product % 10 + '0');
 			}
 		}
+	}
 		_putchar('\n');
 	}
 }	
