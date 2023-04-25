5941821e5d59f9a7a3aed4e76a9e49db8a740e6d
Nefertiti <fredrickamanda3@gmail.com>
2023-04-25 08:44:06 -0700

    Task 10 and 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,9 +4,11 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-	for (int i = n; i <= 98; i++)
+	for (i = n; i <= 98; i++)
 	{
 		printf("%d", i);
 
@@ -19,7 +21,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
