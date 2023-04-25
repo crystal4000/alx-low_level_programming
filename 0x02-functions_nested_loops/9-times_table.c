b471617e27edc7956f3fb5ee364ad37b904986e1
Nefertiti <fredrickamanda3@gmail.com>
2023-04-25 08:17:31 -0700

    Times Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - times table function
+ *
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int product = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			} else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			} else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
