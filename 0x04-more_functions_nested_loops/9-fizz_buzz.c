f89c4482673fbf1c946704441badabea1ca98b21
Nefertiti <fredrickamanda3@gmail.com>
2023-04-27 13:02:48 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
 		{
-			printf("Fizz ");
+			printf("%d", i);
 		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+		if (i != 100)
+			printf(" ");
 	}
-
 	printf("\n");
 	return (0);
 }
