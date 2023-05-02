ebba868f6f4fc290bd11ec77d7291b299391d367
Nefertiti <fredrickamanda3@gmail.com>
2023-05-02 13:03:36 -0700

    Task 7 correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,15 @@
 void puts_half(char *str)
 {
 	int length = _strlen(str);
-	int start_index;
+	int start_index = 0;
 	int i;
 
-	if (length % 2 == 0)
+	if (length % 2 != 0)
 	{
-		start_index = length / 2;
-	}
-	else
-	{
-		start_index = (length - 1) / 2;
+		start_index += 1;
 	}
 
-	for (i = start_index; i < length; i++)
+	for (i = (length + start_index) / 2; i < length; i++)
 	{
 		_putchar(str[i]);
 	}
