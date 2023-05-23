498133c4a0728785f9294ddacfba6ed01f2d7921
Nefertiti <fredrickamanda3@gmail.com>
2023-05-23 13:53:05 -0700

    Tasks DOne

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - Entry point of the program
  *
  * Description: This function prints the name of the file it was compiled from.
- *              It uses the __FILE__ macro to retrieve the file name.
+ * It uses the __FILE__ macro to retrieve the file name.
  *
  * Return: 0 (Success)
  */
